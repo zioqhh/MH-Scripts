2dffa81df4832aaaceb08f650c24897387373b4b
jeeuejje <67346772+jeeuejje@users.noreply.github.com>
2023-08-04 02:28:28 -0400

    Create key.lua

---
+++ key.lua
@@ -0,0 +1,45 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Key", HidePremium = false,IntroTex = "Loading...",SaveConfig = false, ConfigFolder = "OrionTest"})
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
+
+local Section = Tab:AddSection({
+	Name = "Get Key"
+})
+_G.key = 'iusfyef87ewasdhua7'
+_G.keycheck = ""
+function key()
+  if _G.keycheck == _G.key then
+     OrionLib:Destroy()
+     loadstring(game:HttpGet(('https://raw.githubusercontent.com/jeeuejje/MH-Scripts/main/main.lua')))()
+  elseif _G.keycheck == "" then
+     OrionLib:MakeNotification({
+	   Name = "Title!",
+	   Content = "Notification content... what will it say??",
+	   Image = "rbxassetid://4483345998",
+	   Time = 5
+})
+  end
+
+Tab:AddTextbox({
+	Name = "Key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+    _G.keycheck = Value
+		key()
+	end	  
+})
+
+OrionLib:Init()
